4e8492d0c256644eaf8358d8de5108cc50a848a5
Frank <frank.phan@helius-software.com>
2022-05-21 23:33:48 +0700

    Add project files.

---
+++ ASP.NET.WEBAPP/Entities/MyAppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace ASP.NET.WEBAPP.Entities
+{
+    public class MyAppDbContext : DbContext 
+    {
+        public MyAppDbContext(DbContextOptions<MyAppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ ASP.NET.WEBAPP/Entities/MyAppDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ASP.NET.WEBAPP.Entities
+{
+    public class MyAppDbContextFactory : IDesignTimeDbContextFactory<MyAppDbContext>
+    {
+        public MyAppDbContext CreateDbContext(string[] args)
+        {
+            var optionBuilder = new DbContextOptionsBuilder<MyAppDbContext>();
+            optionBuilder.UseSqlServer("Server=DESKTOP-QLHOVH5\\DATHUNG;Initial Catalog=ASPNETAPP;User Id=dathung091;Password=12344321;");
+            return new MyAppDbContext(optionBuilder.Options);
+        }
+    }
+}
